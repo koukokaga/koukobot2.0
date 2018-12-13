72ec142b5ea0064008bcb3b0119da55bb76970e2
koukokaga <koukokaga2000@gmail.com>
2018-12-13 15:59:41 +0100

    firs sesh

--- koukobot.py
+++ koukobot.py
@@ -2,40 +2,39 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-import asyncio
 
 
 bot = commands.Bot(command_prefix='#')
+bot.remove_command('help')
 
 
 @bot.event
 async def on_ready():
     print ("Bot reddy")
-    print ("I am running on " + bot.user.name)
-    print ("With the ID: " + bot.user.id)
 
 
 @bot.command(pass_context=True)
 async def ping(ctx):
-    await bot.say(":ping_pong:")
+    await ctx.send(':ping_pong:')
     print ("user has pinged")
 
 
 @bot.command(pass_context=True)
 async def info(ctx, user: discord.Member):
-    embed = discord.Embed(title="{}'s info".format(user.name), description="here is your information", color=0x00FFFF)
-    await bot.say("name is: {}".format(user.name))
-    await bot.say("users ID is: {}".format(user.id))
-    await bot.say("highest role is: {}".format(user.top_role))
-    await bot.say("joined at: {}".format(user.joined_at))
+    embed = discord.Embed(title="{}'s info".format(user.name), description="", color=0x00FFFF)
+    embed.add_field(name='name', value=user.name)
+    embed.add_field(name='id', value=user.id)
+    embed.add_field(name="Status", value=user.status)
+    embed.add_field(name="Highest role", value=user.top_role)
     embed.set_thumbnail(url=user.avatar_url)
-    await bot.say(embed=embed)
+    await ctx.send(embed=embed)
 
 @bot.command(pass_context=True)
-async def embed(ctx):
-    embed = discord.Embed(title="info about the bot", description="this is my description", color=0x00FFFF)
-    embed.set_footer(text="this is my little project hehe")
-    embed.set_author(name="kouko kaga")
-    
+async def help(ctx):
+    embed = discord.Embed(title="Did you need soem help?", color=0x00FFFF)
+    embed.add_field(name='help', value='shows this')
+    await ctx.send(embed=embed)
 
-bot.run("TOKEN")
+
+
+bot.run("my token")
